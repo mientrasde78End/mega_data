08efbcaab865aa26eda157ddb59c2c530c60563f
Endks <mientrasde78@gmail.com>
2026-02-27 12:47:15 -0500

    projects.models

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -7,7 +8,7 @@ class Project(models.Model):
     tech = models.JSONField()
     repo = models.URLField()
     demo = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to="projects/", null=True)
+    image = CloudinaryField('image', folder='projects', null=True, blank=True)
 
     def __str__(self):
         return self.title
