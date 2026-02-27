3de49dc53086db952a819f03c279a69211ce0fad
Endks <mientrasde78@gmail.com>
2026-02-27 11:04:52 -0500

    configuracion settings

--- core/settings.py
+++ core/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # =========================
 # CLOUDINARY 
