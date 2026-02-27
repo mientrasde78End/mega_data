600b2360ad5c8ef73414620b4fdd87dca256e966
Endks <mientrasde78@gmail.com>
2026-02-27 13:04:59 -0500

    def para imagenes en projects.serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,3 +5,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url  
+        return None
