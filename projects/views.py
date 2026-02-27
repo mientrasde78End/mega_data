2ad08130c1ea4171daced10db4c2262a262392ae
Endks <mientrasde78@gmail.com>
2026-02-26 19:06:02 -0500

    permissions

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,9 @@
 from rest_framework.generics import ListAPIView
 from .models import Project
 from .serializers import ProjectSerializer
-from rest_framework.permissions import allow_any
+from rest_framework.permissions import AllowAny
 
 class ProjectListView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [allow_any]
+    permission_classes = [AllowAny]
