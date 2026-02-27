48ef562f8e2a9a55d913578544f6c1eaf1fe398b
Endks <mientrasde78@gmail.com>
2026-02-27 13:22:23 -0500

    serializer otra vez

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()  
+ 
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = "__all__"
 
     def get_image(self, obj):
+       
         if obj.image:
-            return obj.image.url 
+            return obj.image.url
         return None
